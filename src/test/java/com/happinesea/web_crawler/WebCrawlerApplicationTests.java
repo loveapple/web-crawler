fa598138359761550b54a51f44ca6046109d6362
Chunli Hao <chunli.hao@gmail.com>
2025-07-07 10:24:52 +0900

    add application junit

--- src/test/java/com/happinesea/web_crawler/WebCrawlerApplicationTests.java
+++ src/test/java/com/happinesea/web_crawler/WebCrawlerApplicationTests.java
@@ -1,14 +1,28 @@
 package com.happinesea.web_crawler;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootTest
 class WebCrawlerApplicationTests {
 
 	@Test
 	void contextLoads() {
-		
+
 	}
 
+	@Test
+	void testSpringApplicationRun() {
+		try (ConfigurableApplicationContext context = WebCrawlerApplication.runApplication(new String[] {})) {
+			// アプリケーションコンテキストが起動しているか
+			assertThat(context).isNotNull();
+			assertThat(context.isActive()).isTrue();
+
+			// Bean が取得できるか確認
+			assertThat(context.getBean(WebCrawlerApplication.class)).isNotNull();
+		}
+	}
 }
