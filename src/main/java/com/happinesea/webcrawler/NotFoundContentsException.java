412e1ecb2a91250790bf4fb724765fcbe3f103cc
Chunli Hao <chunli.hao@gmail.com>
2025-08-01 20:16:34 +0900

    add test case for SiteContentsServiceTest

--- src/main/java/com/happinesea/webcrawler/NotFoundContentsException.java
+++ src/main/java/com/happinesea/webcrawler/NotFoundContentsException.java
@@ -2,25 +2,27 @@ package com.happinesea.webcrawler;
 
 public class NotFoundContentsException extends Exception {
 
-	public NotFoundContentsException() {
-		super();
-	}
+	private static final long serialVersionUID = 8456736524587331222L;
+
+//	public NotFoundContentsException() {
+//		super();
+//	}
 
 	public NotFoundContentsException(String message) {
 		super(message);
 	}
 
-	public NotFoundContentsException(Throwable cause) {
-		super(cause);
-	}
+//	public NotFoundContentsException(Throwable cause) {
+//		super(cause);
+//	}
 
 	public NotFoundContentsException(String message, Throwable cause) {
 		super(message, cause);
 	}
 
-	public NotFoundContentsException(String message, Throwable cause, boolean enableSuppression,
-			boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-	}
+//	public NotFoundContentsException(String message, Throwable cause, boolean enableSuppression,
+//			boolean writableStackTrace) {
+//		super(message, cause, enableSuppression, writableStackTrace);
+//	}
 
 }
--- src/test/java/com/happinesea/webcrawler/service/SiteContentsServiceTest.java
+++ src/test/java/com/happinesea/webcrawler/service/SiteContentsServiceTest.java
@@ -1,7 +1,8 @@
 package com.happinesea.webcrawler.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,9 +16,13 @@ import com.happinesea.webcrawler.Const.ProcessStatus;
 import com.happinesea.webcrawler.entity.SiteCategory;
 import com.happinesea.webcrawler.entity.SiteContents;
 import com.happinesea.webcrawler.entity.SiteInfo;
+import com.happinesea.webcrawler.entity.SiteInfoProcessPool;
 import com.happinesea.webcrawler.repository.SiteCategoryRepository;
 import com.happinesea.webcrawler.repository.SiteContentsRepository;
+import com.happinesea.webcrawler.repository.SiteInfoProcessRepository;
 import com.happinesea.webcrawler.repository.SiteInfoRepository;
+
+import jakarta.transaction.Transactional;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -34,12 +39,17 @@ class SiteContentsServiceTest {
 
 	@Autowired
 	private SiteInfoRepository siteInfoRepository;
-	
+
+	@Autowired
+	private SiteInfoProcessRepository siteInfoProcessRepository;
+
 	List<SiteContents> contentsList;
 
 	private SiteCategory category1;
 
 	private SiteCategory category2;
+
+	private SiteInfoProcessPool pool;
 
 	@BeforeEach
 	void setUp() throws Exception {
@@ -47,12 +57,14 @@ class SiteContentsServiceTest {
 
 		SiteInfo site = new SiteInfo();
 		category1 = new SiteCategory();
+		category1.setCategoryName("name1");
 		category1.setSiteInfo(siteInfoRepository.save(site));
 		category1 = siteCategoryRepository.save(category1);
 
 		category2 = new SiteCategory();
+		category2.setCategoryName("name2");
 		category2.setSiteInfo(siteInfoRepository.save(site));
-		siteCategoryRepository.save(category2);
+		category2 = siteCategoryRepository.save(category2);
 
 		SiteContents s1 = new SiteContents();
 		s1.setProcessStatus(ProcessStatus.NONE);
@@ -79,6 +91,13 @@ class SiteContentsServiceTest {
 		contentsList.add(s3);
 		contentsList.add(s4);
 
+		pool = new SiteInfoProcessPool();
+		pool.setSiteInfoProcessId(1);
+		pool.setProcessStatus(ProcessStatus.NONE);
+		pool.setProcessTime(LocalDateTime.now());
+		pool.setSiteCategory(category1);
+
+		pool = siteInfoProcessRepository.save(pool);
 	}
 
 	@Test
@@ -86,8 +105,67 @@ class SiteContentsServiceTest {
 		siteContentsService.bulkInsertIfNotExists(contentsList);
 
 		List<SiteContents> result = siteContentsRepository.findAll();
-		
+
 		assertEquals(4, result.size());
+	}
+
+	@Test
+	@Transactional
+	void testFindAliveProcess() {
+		SiteInfoProcessPool pool2 = new SiteInfoProcessPool();
+		pool2.setSiteInfoProcessId(2);
+		pool2.setProcessStatus(ProcessStatus.PROCESSING);
+		pool2.setProcessTime(LocalDateTime.now());
+		pool2.setSiteCategory(category2);
+		siteInfoProcessRepository.save(pool2);
+
+		List<SiteInfoProcessPool> result = siteContentsService.findAliveProcess();
+
+		assertEquals(1, result.size());
+		SiteInfoProcessPool v = result.get(0);
+		assertEquals(1, v.getSiteInfoProcessId());
+		assertEquals(ProcessStatus.NONE, v.getProcessStatus());
+		assertEquals("name1", v.getSiteCategory().getCategoryName());
+	}
+
+	@Test
+	void testChangSiteInfoProcess2Processing() {
+		SiteInfoProcessPool result = siteContentsService.changSiteInfoProcess2Processing(pool);
+		assertEquals(ProcessStatus.PROCESSING, result.getProcessStatus());
+	}
+
+	@Test
+	void testChangSiteInfoProcess2Sucess() {
+		SiteInfoProcessPool result = siteContentsService.changSiteInfoProcess2Sucess(pool);
+		assertEquals(ProcessStatus.SUCCESS, result.getProcessStatus());
+
+	}
+
+	@Test
+	void testChangSiteInfoProcess2Fail() {
+		SiteInfoProcessPool result = siteContentsService.changSiteInfoProcess2Fail(pool);
+		assertEquals(ProcessStatus.FAIL, result.getProcessStatus());
+
+	}
+
+	@Test
+	void testSaveAllProcessPools() {
+		SiteInfoProcessPool pool2 = new SiteInfoProcessPool();
+		pool2.setSiteInfoProcessId(2);
+		pool2.setProcessStatus(ProcessStatus.PROCESSING);
+		pool2.setProcessTime(LocalDateTime.now());
+		pool2.setSiteCategory(category2);
+		pool2 = siteInfoProcessRepository.save(pool2);
+		
+		List<SiteInfoProcessPool> p = new ArrayList<SiteInfoProcessPool>();
+		p.add(pool);
+		p.add(pool2);
+		
+		List<SiteInfoProcessPool> result = siteContentsService.saveAllProcessPools(p);
+		
+		assertEquals(1, result.size());
+		assertEquals(ProcessStatus.SUCCESS, result.get(0).getProcessStatus());
+		
 	}
 
 }
